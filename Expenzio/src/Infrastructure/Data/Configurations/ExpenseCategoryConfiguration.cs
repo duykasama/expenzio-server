4038bdbb313c960874370ac8169e96c8665ddf7a
duykasama <duykasama@gmail.com>
2024-05-07 19:15:32 +0700

    feat: entity database mappings

--- Expenzio/src/Infrastructure/Data/Configurations/ExpenseCategoryConfiguration.cs
+++ Expenzio/src/Infrastructure/Data/Configurations/ExpenseCategoryConfiguration.cs
@@ -8,8 +8,30 @@ public class ExpenseCategoryConfiguration : IEntityTypeConfiguration<ExpenseCate
 {
     public void Configure(EntityTypeBuilder<ExpenseCategory> builder)
     {
-        builder.Property(t => t.Description)
-            .HasMaxLength(200)
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+        builder.Property(t => t.Name)
+            .HasColumnName("name")
+            .HasMaxLength(100)
             .IsRequired();
+        builder.Property(t => t.UserId)
+            .HasColumnName("user_id")
+            .IsRequired();
+        builder.Property(t => t.Description)
+            .HasColumnName("description")
+            .HasMaxLength(256)
+            .IsRequired();
+        builder.Property(t => t.Created)
+            .HasColumnName("created")
+            .IsRequired();
+        builder.Property(t => t.LastModified)
+            .HasColumnName("last_modified")
+            .IsRequired();
+        builder.Property(t => t.CreatedBy)
+            .HasColumnName("created_by");
+        builder.Property(t => t.LastModifiedBy)
+            .HasColumnName("last_modified_by");
     }
 }
--- Expenzio/src/Infrastructure/Data/Configurations/ExpenseConfiguration.cs
+++ Expenzio/src/Infrastructure/Data/Configurations/ExpenseConfiguration.cs
@@ -8,8 +8,41 @@ public class TodoItemConfiguration : IEntityTypeConfiguration<Expense>
 {
     public void Configure(EntityTypeBuilder<Expense> builder)
     {
-        builder.Property(t => t.Description)
-            .HasMaxLength(200)
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+        builder.Property(t => t.Amount)
+            .HasColumnName("amount")
             .IsRequired();
+        builder.Property(t => t.Description)
+            .HasColumnName("description")
+            .HasMaxLength(256);
+        builder.Property(t => t.MonetaryUnit)
+            .HasColumnName("monetary_unit")
+            .IsRequired();
+        builder.Property(t => t.IsDeleted)
+            .HasColumnName("is_deleted")
+            .HasDefaultValue(false);
+        builder.Property(t => t.UserId)
+            .HasColumnName("user_id")
+            .IsRequired();
+        builder.Property(t => t.CategoryId)
+            .HasColumnName("category_id")
+            .IsRequired();  
+        builder.Property(t => t.Created)
+            .HasColumnName("created")
+            .IsRequired();
+        builder.Property(t => t.LastModified)
+            .HasColumnName("last_modified")
+            .IsRequired();
+        builder.Property(t => t.CreatedBy)
+            .HasColumnName("created_by");
+        builder.Property(t => t.LastModifiedBy)
+            .HasColumnName("last_modified_by");
+        builder.HasOne(t => t.Category)
+            .WithMany(t => t.Expenses)
+            .HasForeignKey(t => t.CategoryId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
